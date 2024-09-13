f56eaf591c00bdfe97e886dbc107908c19d27388
Wesley Eduardo <111645247+wesleyeduardodev@users.noreply.github.com>
2024-09-13 08:41:55 -0300

    Update executar-projeto.sh

--- executar-projeto.sh
+++ executar-projeto.sh
@@ -1,10 +1,9 @@
 #!bin/bash
 
-
 echo ""
 echo "Subindo containers..."
 docker compose -f docker-compose.yml up -d
-echo "Containers OK!"
+echo "Containers gerado!"
 
 echo ""
-read -p "Pressione enter para encerrar..."
+read -p "Pressione enter para encerrar e aguarde um tempinho para que os containers iniciem completamente...Visite o arquivo README.md com instruções de uso do projeto."
